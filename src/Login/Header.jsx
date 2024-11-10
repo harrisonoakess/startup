9471dcfb17fd23b8a7ea74ff447583b6be685352
harrisonoakess <oakeshjo00@gmail.com>
2024-11-09 22:35:48 -0700

    fixed the header so it switches betwenn the pages correctly

--- src/Login/Header.jsx
+++ src/Login/Header.jsx
@@ -1,18 +1,18 @@
 import React from "react";
 import { Link } from "react-router-dom";
+// import button 
+
 
 export default function Header() {
   return (
     <header className="navbar navbar-expand-lg navbar-light bg-light">
       <div className="container-fluid">
         <div className="left_group">
-          <Link to="/chat" className="btn btn-primary">
-            My Tasks
-          </Link>
+          <Link to="/tasks" className="btn btn-primary">My Tasks</Link>
         </div>
         <div className="right_group">
-          <button className="btn btn-secondary me-2">Account</button>
-          <button className="btn btn-outline-danger">Logout</button>
+          <Link to="/" className="btn btn-secondary me-2">Account</Link>
+          <Link to="/" className="btn btn-outline-danger">Logout</Link>
         </div>
       </div>
     </header>
--- src/app.jsx
+++ src/app.jsx
@@ -4,6 +4,7 @@ import Header from "./Login/Header";
 import Footer from "./Login/Footer";
 import LoginForm from "./Login/LoginForm";
 import EmployeeChat from "./Login/EmployeeChat";
+// import 'bootstrap/dist/css/bootstrap.min.css';
 import './app.css';
 
 export default function App() {
@@ -12,7 +13,7 @@ export default function App() {
       <Header />
       <Routes>
         <Route path="/" element={<LoginForm />} />
-        <Route path="/chat" element={<EmployeeChat />} />
+        <Route path="/tasks" element={<EmployeeChat />} />
       </Routes>
       <Footer />
     </Router>
